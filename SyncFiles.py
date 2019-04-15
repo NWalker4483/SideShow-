3d17a3e9fec32f1a81ae64d8e786171279de97b9
NWalker4483 <niwal7@morgan.edu>
2019-04-15 15:05:15 -0400

    sync

--- SyncFiles.py
+++ SyncFiles.py
@@ -1,26 +1,28 @@
 import os 
 import time 
-import subprocesss
+import subprocess as sp
 
-Photo_dir = ""
+#Photo_dir = ""
 Slide_Folder_ID = ""
 #### Helper
-
+Converters = {}
 #### Check Dir
-if not (os.getcwd() == '/home/pi'){
-    print()
+if not (os.getcwd() == '/home/pi/SideShow-'){
+    print("Wrong Directory")
     exit()
 }
-
 #### Download new files 
-os.system("gdrive sync download {} .".format(Slide_Folder_ID))
+while true:
+    os.system("gdrive sync download {} .".format(Slide_Folder_ID))
+    time.sleep(.5)
 #### Convert new files
 for phile in os.listdir("."): 
     try:
-        pass
+        extension = os.path.splitext(phile)[1]
+        
     except:
         print("Invalid File Type present")
 
 #### Launch SlideShow 
-    sp.Popen(['feh', '-Y', '-x', '-q', '-D', '5', '-B', 'black', '-F', '-Z', '-z', '-r', '/media/'])
+    sp.Popen(['feh', '-Y', '-x', '-q', '-D', '5', '-B', 'black', '-F', '-Z', '-z', '-r', "/home/pi/SideShow-/slides"])
     
