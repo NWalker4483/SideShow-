36eeb247a39912538f47b69fe0c6c04a3fe523a5
NWalker4483 <niwal7@morgan.edu>
2019-04-17 20:58:20 -0400

    Sync

--- SyncFiles.py
+++ SyncFiles.py
@@ -13,16 +13,18 @@ if not (os.getcwd() == '/home/pi/SideShow-'){
 }
 #### Download new files 
 while true:
-    os.system("gdrive sync download {} .".format(Slide_Folder_ID))
+    resp = commands.getoutput("rclone copy remote:slides/ /home/pi/SideShow-/slides" ,shell = True)
+    if "403" not in resp:
+        break 
     time.sleep(.5)
 #### Convert new files
 for phile in os.listdir("."): 
     try:
         extension = os.path.splitext(phile)[1]
-        
     except:
         print("Invalid File Type present")
 
 #### Launch SlideShow 
     sp.Popen(['feh', '-Y', '-x', '-q', '-D', '5', '-B', 'black', '-F', '-Z', '-z', '-r', "/home/pi/SideShow-/slides"])
-    
+    time.sleep(10)
+#### Restart the process
